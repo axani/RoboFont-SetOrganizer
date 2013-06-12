f32672b8386db66393039f24fc242f5d4f8816bc
Filip Axani <filip.axani@gmail.com>
2013-06-12 09:19:18 +0200

    Basic window behaves correctly

--- SetOrganizer_basics.py
+++ SetOrganizer_basics.py
@@ -8,12 +8,13 @@ class SetOrganizer:
         self.w = Window((400,400), 'SetOrganizer')
         
         # Basic checkbox
-        self.w.checkBox = CheckBox((10, 10, -10, -10), 'Label', callback=self.checkBoxCallback, value=True)
+        self.w.checkBox1 = CheckBox((10, 10, -10, -10), 'Label', callback=self.checkBoxCallback, value=True)
+        self.w.checkBox2 = CheckBox((10, 35, -10, -10), 'Label', callback=self.checkBoxCallback, value=True)
         
         # Open window
         self.w.open()
     
     def checkBoxCallback(self, sender):
-        print sender.get()
+        print sender.get(), sender.getTitle()
 
 SetOrganizer()
